d8ab14ec2e8f544cc9f03141067226de282667be
lakshmikanth <kanth.explorer@gmail.com>
2018-12-07 02:14:21 +0800

    configuration changes

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ mongoose.connect(process.env.DATABASE_URL)
     .then(() => {
         console.log("Successfully connected to the database");
     }).catch(err => {
-    console.log('Could not connect to the database. Exiting now...');
+    console.log('Could not connect to the database. Exiting now...'+err);
     process.exit();
 });
 
