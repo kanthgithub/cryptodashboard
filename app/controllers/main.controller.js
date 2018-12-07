247e48360df59b921c89936fb62359a37aea33b3
lakshmikanth <kanth.explorer@gmail.com>
2018-12-07 08:15:00 +0800

    Procfile for heroku

--- app/controllers/main.controller.js
+++ app/controllers/main.controller.js
@@ -2,8 +2,6 @@ module.exports = {
     //show home page
     showHome: (request,response)  => {
 
-       var userName = request.parameters.name;
-
-        response.send(`hello ${userName} - welcome to CryptoDashBoard`);
+        response.send(`hello - welcome to CryptoDashBoard`);
     }
 };
