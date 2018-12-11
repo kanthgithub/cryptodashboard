dfc65c24a04abed50b9b0a107cf3671b91339298
lakshmikanth <kanth.explorer@gmail.com>
2018-12-12 01:17:55 +0800

    server.js configurtation for cors

--- server.js
+++ server.js
@@ -20,82 +20,11 @@ const express = require('express'),
     port = process.env.PORT || 3000,
     expressLayouts = require('express-ejs-layouts');
 
+
 const cors = require('cors');
 
-// hard coded configuration object
-conf = {
-    // look for PORT environment variable,
-    // else look for CLI argument,
-    // else use hard coded value for port 8080
-    port: process.env.PORT || process.argv[2] || 8080,
+app.use(cors({origin: '*'}));
 
-    // origin undefined handler
-    // see https://github.com/expressjs/cors/issues/71
-    originUndefined: function (req, res, next) {
-
-        if (!req.headers.origin) {
-
-            res.json({
-
-                mess: 'Hi you are visiting the service locally. If this was a CORS the origin header should not be undefined'
-
-            });
-
-        } else {
-
-            next();
-
-        }
-
-    },
-
-    // Cross Origin Resource Sharing Options
-    cors: {
-
-        // origin handler
-        origin: function (origin, cb) {
-
-            // setup a white list
-            let wl = ['http://localhost:3000','socket.io'];
-
-            if (wl.indexOf(origin) != -1) {
-                console.log("whitelisted origin: "+origin)
-                cb(null, true);
-
-            } else {
-
-                cb(new Error('invalid origin: ' + origin), false);
-
-            }
-
-        },
-
-        optionsSuccessStatus: 200
-
-    }
-
-};
-
-// use origin undefined handler, then cors for all paths
-app.use(cors(conf.cors));
-
-
-var allowCrossDomain = function(req, res, next) {
-    //res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    //res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-    res.header('Access-Control-Allow-Credentials', 'true');
-
-
-    // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-        res.send(200);
-    }
-    else {
-        next();
-    }
-};
-
-app.use(allowCrossDomain);
 
 let morgan = require('morgan');
 
@@ -180,8 +109,9 @@ httpServer.listen(port)
 var marketStreamer = require("./app/service/marketdataStreamer");
 marketStreamer.startWebSocketServer(wss);
 
-//import ticker data from coinmarketcap-API to database
+//import ticker data from coinbase-API to database
 var refreshTool = require("./app/controllers/tickerdata.controller");
 refreshTool.loadTickerData();
+
 
 module.exports = app;
